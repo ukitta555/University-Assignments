f9b067b7d287947dea65ad97f2cfd6fdfc80b82a
Vlad <ukitta555@gmail.com>
2022-04-02 22:00:37 +0300

    Remove smart file

---
+++ Computational Geometry(Python)/SegmentIntersection/runtime_demo/correctness_demo/read_segments/read_segments_from_file.py
@@ -0,0 +1,28 @@
+import logging
+from decimal import Decimal
+from typing import Any
+logger = logging.getLogger(__name__)
+
+
+class ReadSegments:
+    def __init__(self):
+        self.segments: list[tuple[Decimal]] = []
+
+    def run(self, filename: str, **kwargs) -> Any:
+        with open(filename, 'r') as file:
+            for line in file:
+                segment = ReadSegments._parse_coordinates(line)
+                self._add_segment(segment)
+        return self.segments
+
+    def _add_segment(self, segment: tuple[Decimal]):
+        self.segments.append(segment)
+        logger.info(f"Saved segment {segment}")
+
+    @staticmethod
+    def _parse_coordinates(line_of_text: str) -> tuple[Decimal]:
+        return convert_tokens_to_coordinates(line_of_text.split())
+
+
+def convert_tokens_to_coordinates(raw_numbers_from_file: list[str]) -> tuple[Decimal]:
+    return tuple(map(Decimal, raw_numbers_from_file))
