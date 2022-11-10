49f03f5783cabbf33d400e9d85c57cabfaf028a0
Vlad <ukitta555@gmail.com>
2022-11-10 08:37:20 -0500

    NumberSynthesizer

---
+++ Ukrainian_Language_Processing_Python/NumberSynthesizer/main.py
@@ -0,0 +1,28 @@
+import logging
+
+import num2words
+from pydub import AudioSegment
+
+logger = logging.getLogger(__name__)
+
+def detect_num_and_convert(word):
+    numbers = "0123456789.-"
+    is_number = all(map(lambda x: x in numbers, word))
+    if is_number:
+        try:
+            return num2words.num2words(word, lang="uk")
+        except Exception as e:
+            print(e)
+            return word
+    else:
+        return word
+
+if __name__ == '__main__':
+    for i in [97563, 1000000, -1000000, 13234.4112, 244719]:
+        combined = AudioSegment.empty()
+        text = detect_num_and_convert(str(i))
+        for part in text.split(" "):
+            voice_part = AudioSegment.from_wav(f'./audio/{part}.wav')
+            combined += voice_part
+
+        combined.export(f"./output/{str(i)}.wav", format="wav")
